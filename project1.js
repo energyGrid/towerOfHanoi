f0efb070b19e426b4c6419b22d4f558386deeb42
Scott Pierce <scottpiercew@gmail.com>
2017-05-15 16:37:52 -0400

    draggable discs

--- project1.js
+++ project1.js
@@ -3,15 +3,21 @@ silver: drag elements to new locations, css, https://api.jqueryui.com/draggable/
 gold: stack of seven elements and a timer*/
 
 // //Create disk elements using jquery.
-// function handleDragStart(e) {
-//   this.style.opacity = "0.8";
-// }
+
 $(init)
 function init() {
   $("#discOne").draggable( {
     cursor: "move",
-    containment: "window",
-  })
+    containment: "window"
+});
+$("#discTwo").draggable( {
+  cursor: "move",
+  containment: "window"
+});
+$("#discThree").draggable( {
+  cursor: "move",
+  containment: "window"
+});
 }
 //function allowing element to move one space right on click event
 
