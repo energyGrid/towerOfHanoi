5b136240dd9742d8ea13c4a429e033ed94233e66
Scott Pierce <scottpiercew@gmail.com>
2017-05-15 15:11:53 -0400

    Was able to drag first item

--- project1.js
+++ project1.js
@@ -2,10 +2,17 @@
 silver: drag elements to new locations, css, https://api.jqueryui.com/draggable/
 gold: stack of seven elements and a timer*/
 
-//Create disk elements using jquery.
-// var $diskOne =
-$("#diskOne").appendTo(".gameMiddle");
-
+// //Create disk elements using jquery.
+// function handleDragStart(e) {
+//   this.style.opacity = "0.8";
+// }
+$(init)
+function init() {
+  $("#discOne").draggable( {
+    cursor: "move",
+    containment: "window",
+  })
+}
 //function allowing element to move one space right on click event
 
 //function allowing element to move two spaces right on click event
