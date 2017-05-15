04125b9987f7b33ef48ca102e304202fed864c11
Scott Pierce <scottpiercew@gmail.com>
2017-05-15 13:51:18 -0400

    jQuery UI

---
+++ project1.js
@@ -0,0 +1,17 @@
+/*bronze: stack of three elements that move right and left by clicking
+silver: drag elements to new locations, css, https://api.jqueryui.com/draggable/
+gold: stack of seven elements and a timer*/
+
+//Create disk elements using jquery.
+// var $diskOne =
+$("#diskOne").appendTo(".gameMiddle");
+
+//function allowing element to move one space right on click event
+
+//function allowing element to move two spaces right on click event
+
+//function allowing element to move one space left on click event
+
+//function allowing element to move two spaces left on click event
+
+//prevent a larger element from landing on-top of a smaller element
